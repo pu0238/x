976d1540766cdda44e51b0ffce6559260648c2aa
Kuba Rzepka <krzepka33@gmail.com>
2024-11-06 03:17:48 +0100

    modal workaround for audio problems & small changes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,17 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 
 const Landing = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   useEffect(() => {
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+
     const trumpAudio = new Audio("/sounds/eagle.mp3");
     const kamalaAudio = new Audio("/sounds/miau.mp3");
 
@@ -10,7 +20,7 @@ const Landing = () => {
 
     const playAudio = (audio: HTMLAudioElement) => {
       audio.currentTime = 0;
-      audio.play().catch((error) => console.log("Error playing audio:", error));
+      audio.play().catch((error: any) => console.log("Error playing audio:", error));
     };
 
     const stopAudio = (audio: HTMLAudioElement) => {
@@ -21,22 +31,39 @@ const Landing = () => {
     const handleMouseOver = (audio: HTMLAudioElement) => () => playAudio(audio);
     const handleMouseOut = (audio: HTMLAudioElement) => () => stopAudio(audio);
 
-    const trumpImage = document.querySelector<HTMLImageElement>(
-      'img[alt="Donald Trump Image"]'
-    );
-    const kamalaImage = document.querySelector<HTMLImageElement>(
+    const trumpImage = document.querySelector('img[alt="Donald Trump Image"]');
+    const kamalaImage = document.querySelector(
       'img[alt="Kamala Harris Image"]'
     );
 
-    if (trumpImage) {
-      trumpImage.addEventListener("mouseover", handleMouseOver(trumpAudio));
-      trumpImage.addEventListener("mouseout", handleMouseOut(trumpAudio));
-    }
+    const enableAudioPlayback = () => {
+      if (audioContext.state === "suspended") {
+        audioContext.resume().then(() => {
+          if (trumpImage) {
+            trumpImage.addEventListener(
+              "mouseover",
+              handleMouseOver(trumpAudio)
+            );
+            trumpImage.addEventListener("mouseout", handleMouseOut(trumpAudio));
+          }
 
-    if (kamalaImage) {
-      kamalaImage.addEventListener("mouseover", handleMouseOver(kamalaAudio));
-      kamalaImage.addEventListener("mouseout", handleMouseOut(kamalaAudio));
-    }
+          if (kamalaImage) {
+            kamalaImage.addEventListener(
+              "mouseover",
+              handleMouseOver(kamalaAudio)
+            );
+            kamalaImage.addEventListener(
+              "mouseout",
+              handleMouseOut(kamalaAudio)
+            );
+          }
+        });
+      }
+
+      document.removeEventListener("click", enableAudioPlayback);
+    };
+
+    document.addEventListener("click", enableAudioPlayback);
 
     return () => {
       if (trumpImage) {
@@ -56,6 +83,7 @@ const Landing = () => {
           handleMouseOut(kamalaAudio)
         );
       }
+      document.removeEventListener("click", enableAudioPlayback);
     };
   }, []);
 
@@ -74,30 +102,62 @@ const Landing = () => {
       </div>
       <div className="absolute top-0 right-0 w-full h-screen z-[20]">
         <div className="relative w-full h-full">
-          <a href="https://pump.fun/2eUBVo6ZD26xZukES8jAFjcD6HUE4K3Ku4h68nbdpump" className="absolute bottom-0 left-0">
+          <a
+            href="https://pump.fun/2eUBVo6ZD26xZukES8jAFjcD6HUE4K3Ku4h68nbdpump"
+            className="absolute bottom-0 left-0"
+          >
             <img
               className="object-cover max-w-[1500px] transition-transform duration-300 ease-in-out hover:scale-110"
               src="/images/trump.png"
               alt="Donald Trump Image"
             />
           </a>
-          <a href="https://pump.fun/2eUBVo6ZD26xZukES8jAFjcD6HUE4K3Ku4h68nbdpump" className="absolute bottom-0 right-0">
+          <a
+            href="https://pump.fun/2eUBVo6ZD26xZukES8jAFjcD6HUE4K3Ku4h68nbdpump"
+            className="absolute -bottom-2 right-0"
+          >
             <img
-              className="object-cover max-w-[1500px] transition-transform duration-300 ease-in-out hover:scale-110"
+              className="object-cover w-[935px] transition-transform duration-300 ease-in-out hover:scale-110"
               src="/images/kamala.png"
               alt="Kamala Harris Image"
             />
           </a>
         </div>
       </div>
-      <div className="absolute top-[10%] z-[20] w-fit h-fit">
+      <div className="absolute top-[10%] z-[20] w-fit h-fit flex flex-col justify-center items-center gap-6">
         <h1 className="font-bold text-center text-white uppercase text-8xl font-josefin">
           PEPE VOTE
         </h1>
         <p className="text-4xl font-bold text-center text-white uppercase font-josefin">
-          Vote now!
+          Choose your PEPESIDENT!
         </p>
+        <a href="https://x.com/PepeVoteToken" target="_blank">
+          <img
+            className="w-[35px] h-auto white-filter"
+            src="/images/icons/x.svg"
+            alt="X"
+          />
+        </a>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Welcome Modal"
+        className="fixed inset-0 flex items-center justify-center z-[30]"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50"
+      >
+        <div className="bg-white p-8 rounded-lg shadow-lg text-center font-josefin flex flex-col gap-4">
+          <img className="w-24 h-auto mx-auto" src="/images/pepe.png" alt="Pepe Vote" />
+          <h2 className="text-2xl font-bold mb-4 ">JOIN OUR PEPE VOTING!</h2>
+          <p className="mb-6">Do you want to vote?</p>
+          <button
+        onClick={closeModal}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors"
+          >
+        Yes
+          </button>
+        </div>
+      </Modal>
     </section>
   );
 };
